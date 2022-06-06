610df5c77deb2225c9df1b28bebcc6c9293223b0
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-06-06 08:54:38 -0300

    up

---
+++ src/funtionmethod1/FuntionMethod1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funtionmethod1;
+
+/**
+ *
+ * @author Marce
+ */
+public class FuntionMethod1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int somaMetod(int a ,int b){
+        int soma = a+b;
+        return soma;
+    } 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = somaMetod(5, 2);
+        System.out.println(sm);
+        
+        
+    }
+    
+}
